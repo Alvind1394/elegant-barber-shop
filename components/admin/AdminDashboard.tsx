3c0c305a488acfa07b6f68e775bae599a43dc696
Alvind1394 <alvind1394@hotmail.es>
2026-06-05 00:35:05 -0400

    Update timer display in AdminDashboard
    Replaced LiveTimer component with a simple active timer message.

--- components/admin/AdminDashboard.tsx
+++ components/admin/AdminDashboard.tsx
@@ -967,24 +967,10 @@ const inProgressCount =
 
             </div>
 
-            {appointment.timer_end && (
-  <LiveTimer
-  timerEnd={
-    appointment.timer_end
-  }
-
-  appointmentId={
-    appointment.id
-  }
-
-  customerName={
-    appointment.customer_name
-  }
-
-  onAutoComplete={
-    onComplete
-  }
-/>
+           {appointment.timer_end && (
+  <div>
+    Timer activo
+  </div>
 )}
 
             <button
